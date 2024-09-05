a33c9b1ed7e755f4f399821d7822cc4fd419a771
Marc064 <marco.varg002@gmail.com>
2024-09-04 19:49:49 -0500

    Add json of Power Rangers and Kamen Raider without links of images

--- controller/sentaiController.js
+++ controller/sentaiController.js
@@ -3,17 +3,41 @@ const fs = require('fs')
 
 seasonsSentai = path.join(__dirname, '../persistence/seasonsSentai.json')
 
-module.exports ={
-    findAll: async(req, res)=>{
+let sentai = []
+try {
+    const data = fs.readFileSync(seasonsSentai, 'utf8')
+    if (data.trim() !== '') {
+        const jsonData = JSON.parse(data)
+        sentai = jsonData
+        console.log(sentai)
+    } else {
+        console.log(`El archivo ${seasonsSentai} está vacío`);
+
+    }
+} catch (error) {
+    console.log(`Error al parcear el archivo JSON ${seasonsSentai}`, error);
+
+}
+
+module.exports = {
+    findAll: async (req, res) => {
         try {
-            let sentai = new Map()
-            try{
-                const data = fs.readFileSync(seasonsSentai, 'utf8')
-            } catch (error) {
-                return res.status(500).json({"state": false, "error": error})
-            }
+            return res.status(200).json({ "state": true, "sentai": sentai })
         } catch (error) {
-            return res.status(500).json({"state": false, "error": error})
+            return res.status(500).json({ "state": false, "error": error.message })
+        }
+    },
+    findById: async (req, res) => {
+        const { id } = req.params
+        const index = parseInt(id, 10)
+
+        try {
+            if (isNaN(index) || index < 0 || index >= sentai.length) {
+                return res.status(404).json({ "state": false, "error": "Elemento no encontrado" })
+            }
+            return res.status(200).json({ "state": true, "sentai": sentai[index] })
+        } catch (error) {
+            return res.status(500).json({ "state": false, "error": error.message })
         }
     }
 }
