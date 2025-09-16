cca1e8afbb0009977ad4bad7719bfc1933773253
Marc064 <marco.varg002@gmail.com>
2025-09-15 20:55:59 -0500

    Controladores para saga

--- src/controller/tokusatsuController.ts
+++ src/controller/tokusatsuController.ts
@@ -2,6 +2,19 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+// Saga Controllers
 export function getAllSagas() {
     return prisma.saga.findMany();
+}
+
+export function getSagaByName(name: string) {
+    console.log(name);
+
+    return prisma.saga.findFirst({
+        where: {
+            saga: {
+                contains: name
+            }
+        }
+    })
 }
--- src/router/tokusatsuRouter.ts
+++ src/router/tokusatsuRouter.ts
@@ -1,12 +1,30 @@
 import express from "express";
-import { getAllSagas } from "../controller/tokusatsuController";
+import {
+    getAllSagas,
+    getSagaByName
+} from "../controller/tokusatsuController";
 
 const router = express.Router();
 
+// Saga Routes
 router.get("/sagas", async (_req, res) => {
     try {
         const types = await getAllSagas();
-        res.json(types);
+        res.status(200).json(types);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+
+
+// Type Routes
+router.get("/saga/:name", async (req, res) => {
+    const { name } = req.params;
+    console.log(name);
+    try {
+        const type = await getSagaByName(name.toUpperCase());
+        res.status(type ? 200 : 404).json(type ? type : { error: "Not Found" });
     } catch (error) {
         res.status(500).json({ error: "Internal Server Error" });
     }
